4b3c6d335773d310981d1905d50f831c688ff7e4
EnzoAmarilla <enzo100amarilla@gmail.com>
2024-10-30 13:31:28 -0300

    quitamos requerido email y validamos dni

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -101,12 +101,16 @@ class PatientController extends Controller
             'name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
             'email' => [
-                'required',
+                'nullable',
                 'string',
                 'email',
                 'max:255',
                 Rule::unique('users')->ignore($id),
             ],
+            'dni' => [
+                'required',
+                Rule::unique('users')->ignore($id),
+            ]
         ]);
     
         if ($validator->fails()) {
